fe8e912e7eb4a65220d6caa6f508c0b6e038bf71
Nnamdi <smithdegreat@hotmail.com>
2019-09-16 20:28:49 +0100

    Application built for deployment

--- src/app/_helper/config.js
+++ src/app/_helper/config.js
@@ -3,15 +3,19 @@ module.exports = {
     url: ""
   },
   development: {
-    url: "http://localhost:3000/api",
-    imageUrl: "http://localhost:3000/uploads/"
+    //url: "http://localhost:3000/api",
+    url: "https://webapi.softnergy.com/api",
+    //imageUrl: "http://localhost:3000/uploads/"
+    imageUrl: "https://webapi.softnergy.com/uploads/"
   },
   getImage: image => {
-    let img = "http://localhost:3000/uploads/";
+    //let img = "http://localhost:3000/uploads/";
+    let img = "https://webapi.softnergy.com/uploads/";
     return `${img}${image}`;
   },
   getImageItem: image => {
-    let img = "http://localhost:3000/Images/";
+    //let img = "http://localhost:3000/Images/";
+    let img = "https://webapi.softnergy.com/Images/";
     return `${img}${image}`;
   }
 };
